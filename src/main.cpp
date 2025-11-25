e86d6fd93fcd3d4fb87b2e84861dbfdd689b432a
inshu5757 <mrs.sharma177@gmail.com>
2025-11-25 15:39:57 +0530

    Added Banker's Algorithm for deadlock detection

--- src/main.cpp
+++ src/main.cpp
@@ -37,5 +37,86 @@ int main() {
 
     cout << "\nINPUT CAPTURED SUCCESSFULLY!\n";
 
+    //--------------------------
+    // Calculate Need matrix
+    //--------------------------
+    
+    vector<vector<int>> need(processes, vector<int>(resources));
+    
+    for(int i=0;i<processes; i++){
+        for(int j=0;j<resources;j++){
+            need[i][j] = maxNeed[i][j] - allocation[i][j];
+        }
+    }
+    
+    
+    //--------------------------
+    // Safety Algorithm
+    //--------------------------
+
+    vector<int> work = available;
+    vector<bool> finish(processes, false);
+    vector<int> safeSequence;
+
+    int count = 0;
+
+    while(count < processes){
+        bool found = false;
+
+        for(int p=0;p< processes;p++){
+            if(!finish[p]){
+                bool flag = true;
+                
+                for(int r=0;r<resources;r++){
+                    if(need[p][r] > work[r]){
+                        flag = false;
+                        break;
+                    }
+                }
+
+                if(flag){
+                    //..Process can run safely
+                    for(int r=0;resources;r++){
+                        work[r] += allocation[p][r];
+                    }
+
+                    finish[p] = true;
+                    safeSequence.push_back(p);
+                    found = true;
+                    count++;
+                }
+            }
+        }
+
+        if(!found){
+            break;
+        }
+    }
+
+    //--------------------------
+    // Check if system is safe
+    //--------------------------
+    bool safe = true;
+    for(int i=0;i<processes;i++){
+        if(!finish[i]){
+            safe = false;
+            break;
+        }
+    }
+
+    if(safe){
+        cout << "\nSystem is in SAFE STSTE.\n";
+        cout << "Safe Sequence: ";
+        for(int i=0;i<processes;i++){
+            cout << "p" << safeSequence[i];
+            if(i != processes-1) cout << " -> ";
+        }
+        cout << "\n";
+    }
+    else{
+        cout << "\nDEADLOCK DETECTED!\n";
+        cout << "No Safe Sequence exists.\n";
+    }
+
     return 0;
 }
